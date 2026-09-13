ff2c42eebe2d34ad33b3a3756235fdc1fc20c3d9
olandusgood-byte <olandusgood@gmail.com>
2026-09-13 01:57:22 -0400

    add content mission progression control

--- game/content-ui.js
+++ game/content-ui.js
@@ -1,1 +1,10 @@
-(() => { document.querySelectorAll('[data-mission]').forEach(b=>b.onclick=()=>{if(window.TGGContent?.start(b.dataset.mission)){window.TGGGame?.show?.('game')}}); document.getElementById('contentBtn').onclick=()=>window.TGGGame?.show?.('contentBoard'); document.getElementById('contentBack').onclick=()=>window.TGGGame?.show?.('game'); })();
+(() => {
+  const $=id=>document.getElementById(id);
+  document.querySelectorAll('[data-mission]').forEach(b=>b.onclick=()=>{
+    if(window.TGGContent?.start(b.dataset.mission)) window.TGGGame?.show?.('contentBoard');
+  });
+  $('contentBtn')?.addEventListener('click',()=>window.TGGGame?.show?.('contentBoard'));
+  $('contentBack')?.addEventListener('click',()=>window.TGGGame?.show?.('game'));
+  $('advanceContentBtn')?.addEventListener('click',()=>window.TGGContent?.advance?.());
+  window.TGGContentUI={advance:()=>window.TGGContent?.advance?.()};
+})();

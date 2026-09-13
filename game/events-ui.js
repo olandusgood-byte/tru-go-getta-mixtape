4a70c12e95b23a2c7503a7711ab51b001c2d8b5a
olandusgood-byte <olandusgood@gmail.com>
2026-09-13 12:05:34 -0400

    V1.4: expose city event summary feedback

--- game/events-ui.js
+++ game/events-ui.js
@@ -2,5 +2,5 @@
   function open(){window.TGGEvents?.render?.();window.TGGGame?.show?.('eventsBoard')}
   document.getElementById('eventsBtn')?.addEventListener('click',open);
   document.getElementById('eventsBack')?.addEventListener('click',()=>window.TGGGame?.show?.('game'));
-  window.TGGEventsUI={open,render:()=>window.TGGEvents?.render?.()};
+  window.TGGEventsUI={open,render:()=>window.TGGEvents?.render?.(),summary:()=>window.TGGEvents?.summary?.()};
 })();

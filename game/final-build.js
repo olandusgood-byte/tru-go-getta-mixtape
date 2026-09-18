ca45ff27431fc1b339f73126e62acdb008bfdb0e
olandusgood-byte <olandusgood@gmail.com>
2026-09-18 17:55:36 -0400

    Version runtime for V2.15 CHARACTER + MISSION DEPTH 100

--- game/final-build.js
+++ game/final-build.js
@@ -1,5 +1,5 @@
 (() => {
-  const VERSION='V2.14 PLAYTEST HARDENING 100';
+  const VERSION='V2.15 CHARACTER + MISSION DEPTH 100';
   const startedAt=Date.now();
   let quality='high';
   let autosaves=0;

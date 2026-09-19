7c94e56f833d5edb0f1fcbf19d3e7eb88530d45a
olandusgood-byte <olandusgood@gmail.com>
2026-09-18 21:16:54 -0400

    Version runtime for V2.39 Stage Performance Forge

--- game/final-build.js
+++ game/final-build.js
@@ -1,5 +1,5 @@
 (() => {
-  const VERSION='V2.38 TGG VEHICLE DAMAGE + INTERIOR FORGE 100';
+  const VERSION='V2.39 TGG STAGE + PERFORMANCE FORGE 100';
   const startedAt=Date.now();
   let quality='high';
   let autosaves=0;
